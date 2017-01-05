ad006e11187060787970e5140304fa4dc9b36eda
Jack <92jackhenderson@gmail.com>
2017-01-05 16:29:14 +0000

    now with more tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,8 +4,11 @@ require 'docking_station'
 describe DockingStation do
   it "releases a bike" do
     expect(subject).to respond_to(:release_bike)
+  end
+
+  it "checks if bike exists and is working" do
     bike = subject.release_bike
-    expect(bike).not_to eq nil
+    #expect(bike).not_to eq nil
     expect(bike).to be_working
   end
 end
