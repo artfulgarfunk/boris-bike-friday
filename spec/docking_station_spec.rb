7057c508b4c4ec5bfb477d840497234691c45505
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-01-06 15:19:16 +0000

    ex 12

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,13 +1,16 @@
 require 'docking_station'
 require 'bike'
 
+bike = Bike.new
+
 describe DockingStation do
   it "releases a bike" do
     expect(subject).to respond_to(:release_bike)
+    expect {subject.release_bike}.to raise_error("No bikes available!")
   end
 
   it "checks if bike exists and is working" do
-    bike = subject.release_bike
+    #bike = subject.release_bike
     expect(bike).to be_working
   end
 
