a38652a0f5891b16acce70210362a2cda0962295
Jack <92jackhenderson@gmail.com>
2017-01-06 14:43:41 +0000

    ex11

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,5 @@
 require 'docking_station'
-#require 'bike'
+require 'bike'
 
 describe DockingStation do
   it "releases a bike" do
@@ -9,5 +9,15 @@ describe DockingStation do
   it "checks if bike exists and is working" do
     bike = subject.release_bike
     expect(bike).to be_working
+  end
+
+  it "respond to #dock" do
+    expect(subject).to respond_to(:dock).with(1).argument
+  end
+
+  it "docks a bike a" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
   end
 end
